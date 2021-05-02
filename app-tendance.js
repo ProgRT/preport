4d493240cb341619f3e8f424b7e09abf105fb38e
ProgRT <nicolas.blais.stlaurent@gmail.com>
2021-05-02 07:06:51 -0400

    Major update. Folder architecture redone.

--- app-tendance.js
+++ app-tendance.js
@@ -3,7 +3,8 @@ import {zeroStartVolume, calcVstart, calcPoints, calcRecr} from './lib/functions
 
 const settings = {
 	langue: 'fr-ca',
-	conditions: [],
+	conditions: [
+	],
 	tableColumns: [
 		{name: "PEP", type: "number", unit: 'hPa'},
 		{name: "Ppl", type: "number", unit: 'hPa'},
@@ -11,20 +12,22 @@ const settings = {
 		{name: 'Vcder', type: 'number', unit: 'ml'},
 		{name: 'Vdiff', type: 'result', unit: 'ml', formule: d=>d.Vcder - d.Vc},
 		{name: "Pmotrice", type: "result", formule: d => d.Ppl - d.PEP, unit: 'cmH2O'},
-		{name: "Cst", type: "result", unit: 'ml/hPa', graph: false, formule: d=>Math.round(10*d.Vc/d.Pmotrice)/10},
+		{name: "Cst", type: "result", unit: 'ml/hPa', graph: true, formule: d=>Math.round(10*d.Vc/d.Pmotrice)/10},
 		{name: 'Vrec', type: 'result'},
 		{name: 'RI', type: 'result'},
-		{name: "SpO2", type: "number", unit: '%', graph: false},
-		{name: "EtCO2", type: "number", unit: 'mmHg', graph: false},
-		{name: "TAM", type: "number", unit: 'mmHg', graph: false},
+		{name: "SpO2", type: "number", unit: '%', graph: true},
+		{name: "EtCO2", type: "number", unit: 'mmHg', graph: true},
+		{name: "TAM", type: "number", unit: 'mmHg', graph: true},
 		{name: "Commentaire", type: "text"},
 	],
 	grconf: {
 		padG: 0.05,
 		padD: 0.05,
 		padH: 0.1,
+		padB: 0.1,
 		margeG: 80,
-		margeB: 60,
+		margeB: 52,
+		margeH: 5
 	},
 	extraRowFunc: [
 		zeroStartVolume, // Lowest PEEP Vstart  = 0
@@ -32,7 +35,7 @@ const settings = {
 		calcPoints,      // Creation of the points fro plotting
 		calcRecr         // Description of the recruitment between PEEP levels
 	],
-	postUpdateFunc: ['updateCgraph']
+	postUpdateFunc: ['updateGraph']
 };
 
 let report = new preport(settings);
